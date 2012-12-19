3a921c5d6d8e2f548d7cae94de59fab4e87cd4dc
Niels Zeilemaker <niels@zeilemaker.nl>
2012-12-19 19:18:41 +0100

    Yet another path fix

--- run_scenario_1000.sh
+++ run_scenario_1000.sh
@@ -33,9 +33,9 @@ cd mainbranch
 echo $PATH
 ls -l ../dispersy_experiments/scripts
 
-das4-start $SCRIPTS_PATH/das4-allchannel.conf ../dispersy_experiments/scenario_1000/ 20 5 $HEAD_IP $TRACKER_PORT $SYNC_PORT
+das4-start $SCRIPTS_PATH/das4-allchannel.conf $PWD/../dispersy_experiments/scenario_1000/ 20 5 $HEAD_IP $TRACKER_PORT $SYNC_PORT
 
-post-process-experiment $SCRIPTS_PATH/das4-allchannel.conf ../dispersy_experiments/scenario_1000/
+post-process-experiment $SCRIPTS_PATH/das4-allchannel.conf $PWD/../dispersy_experiments/scenario_1000/
 
 cd ..
 
