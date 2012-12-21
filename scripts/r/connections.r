0bcd44c0ecbcdca3328a4382385555206e9d450e
Niels Zeilemaker <niels@zeilemaker.nl>
2012-12-21 15:59:41 +0100

    Connections plot

---
+++ scripts/r/connections.r
@@ -0,0 +1,21 @@
+.libPaths( c("~/R/x86_64-redhat-linux-gnu-library/2.15", .libPaths()))
+is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1]) 
+
+toInstall <- c("ggplot2", "reshape")
+for (package in toInstall){
+	if (is.installed(package) == FALSE){
+		install.packages(package, repos = "http://cran.r-project.org", lib="~/R/x86_64-redhat-linux-gnu-library/2.15")		
+	}
+}
+lapply(toInstall, library, character.only = TRUE)
+
+df <- read.table("total_connections_1_reduced.txt", header = TRUE, check.names = FALSE)
+df <- melt(df, id="time")
+
+p <- ggplot(df, aes(time, value, group=variable, colour=variable, linetype=variable)) + theme_bw()
+p <- p + geom_line(data = df, alpha = 0.5)
+p <- p + opts(legend.position="none")
+p <- p + labs(x = "\nTime into experiment (Seconds)", y = "Connections per peer\n")
+p
+
+ggsave(file="total_connections_1.png", width=8, height=6, dpi=100)
