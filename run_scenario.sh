b0a45b33293d4dd90a9349fae22aab968d32cb01
Niels Zeilemaker <niels@zeilemaker.nl>
2013-01-04 16:15:58 +0100

    Debug stuff

--- run_scenario.sh
+++ run_scenario.sh
@@ -24,11 +24,14 @@ sed -i "s/^COMMUNITY\_ARGS=.*/COMMUNITY\_ARGS=$COMMUNITY_ARGS/" "$EXPERIMENT_CON
 
 #Start experiment
 das4-start $EXPERIMENT_CONFIG $PWD/../dispersy_experiments/$SCENARIO_PATH/ $NRNODES $DURATION $TRACKER_PORT
-XRANGE="$(post-process-experiment $EXPERIMENT_CONFIG $PWD/../dispersy_experiments/$SCENARIO_PATH/)"
+post-process-experiment $EXPERIMENT_CONFIG $PWD/../dispersy_experiments/$SCENARIO_PATH/
+
+#XRANGE is now in experiment config
+source $EXPERIMENT_CONFIG
 
 cd ../dispersy_experiments/$SCENARIO_PATH/output
 
-R --no-save --quiet --args "$XRANGE" < $WORKSPACE/dispersy_experiments/scripts/r/drop.r &
+R --no-save --quiet --args $XRANGE < $WORKSPACE/dispersy_experiments/scripts/r/drop.r &
 PID1=$! 
 R --no-save --quiet --args $XRANGE < $WORKSPACE/dispersy_experiments/scripts/r/total_records.r &
 PID2=$!
