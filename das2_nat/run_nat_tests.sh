b4aafe636b542233e825e730109b85c52fdfeb0b
Elric Milon <whirm@gmx.com>
2013-01-24 17:16:52 +0100

    create a specific dir for the nat experiment

--- das2_nat/run_nat_tests.sh
+++ das2_nat/run_nat_tests.sh
@@ -5,6 +5,8 @@ set -e
 #svn co https://svn.tribler.org/dispersy/branches/20120612-27046-mainbranch dispersy
 #svn co https://svn.tribler.org/abc/branches/boudewijn/das4-walker das4_walker
 
+HOSTLIST=$(dirname $(readlink -f $0))/hostlist.txt
+
 if [ ! -e dispersy -o ! -e das4_walker ]; then
     echo "Please, check out dispersy and das4_walker before running this script."
     exit 1
@@ -17,20 +19,20 @@ echo Deleting old data
 rm -fR output
 
 echo Deleting old code
-parallel-ssh -p 40 -h hostlist.txt rm -fR /home/whirm/dispersy /home/emilon/das4_walker /home/emilon/mainbranch
+parallel-ssh -p 40 -h $HOSTLIST rm -fR /home/whirm/dispersy /home/emilon/das4_walker /home/emilon/mainbranch
 
 echo Copying dispersy
-parallel-rsync -v -o out -e out -p 40 -h hostlist.txt -avz dispersy /home/emilon/dispersy
+parallel-rsync -v -o out -e out -p 40 -h $HOSTLIST -avz dispersy /home/emilon/dispersy
 
 echo Copying das4-walker
-parallel-rsync -p 40 -h hostlist.txt -avz das4_walker/ /home/emilon/das4_walker
+parallel-rsync -p 40 -h $HOSTLIST -avz das4_walker/ /home/emilon/das4_walker
 
 echo Running the experiment
-parallel-ssh -t 0 -i -p 40 -h hostlist.txt 'cd das4_walker && pwd && python -c "from dispersy.tool.main import main; main()" --script walker.script.ScenarioScript --kargs peernumber=$(hostname |cut -f2 -de),scenario=config'
+parallel-ssh -t 0 -i -p 40 -h $HOSTLIST 'cd das4_walker && pwd && python -c "from dispersy.tool.main import main; main()" --script walker.script.ScenarioScript --kargs peernumber=$(hostname |cut -f2 -de),scenario=config'
 
 echo Getting experiment data back from the nodes
 mkdir -p output
-for HOST in $(grep -v ^# hostlist.txt); do
+for HOST in $(grep -v ^# $HOSTLIST); do
     rsync --delete -a $HOST:das4_walker/walktest.log output/$HOST.walktest.log
 done
 
