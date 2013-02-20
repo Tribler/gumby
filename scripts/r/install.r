85f7ad3c7f7a11c5ac4b2f3adc8ca1dbbfc0d1b6
Elric Milon <whirm@gmx.com>
2013-02-20 11:16:09 +0100

    Add new dependencies for the NAT experiment

--- scripts/r/install.r
+++ scripts/r/install.r
@@ -1,7 +1,7 @@
 .libPaths( c("~/R/x86_64-redhat-linux-gnu-library/2.15", .libPaths()))
 is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1]) 
 
-toInstall <- c("ggplot2", "reshape")
+toInstall <- c("ggplot2", "reshape", "gplots", "plotrix")
 for (package in toInstall){
 	if (is.installed(package) == FALSE){
 		install.packages(package, repos = "http://cran.r-project.org", lib="~/R/x86_64-redhat-linux-gnu-library/2.15")		
