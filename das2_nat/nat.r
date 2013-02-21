87b7063971f02112392e9ecf47e1ce04be4937dc
Niels Zeilemaker <niels@zeilemaker.nl>
2013-02-21 17:52:51 +0100

    New sorting

--- das2_nat/nat.r
+++ das2_nat/nat.r
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(reshape)
 library(stringr)
+library(grid)
 
 r <- read.table("request.data", header=TRUE, row.names=1)
 s <- read.table("success.data", header=TRUE, row.names=1)
@@ -22,6 +23,7 @@ d <- merge(rm, srm, sort=FALSE)
 d <- na.omit(d)
 
 d$to <- str_sub(d$to, 0, -3)
+d$to <- factor(d$to, levels = unique(d[with(d, order(-as.numeric(nattype), from)),]$from))
 
 nattypes <- list('1'="Symmetric", '2'="Port Res", '3'="Restricted", '4'="Full Cone", '5'="Open", '6' = "Unknown", '9'="?")
 mf_labeller <- function(var, value){
@@ -30,11 +32,12 @@ mf_labeller <- function(var, value){
 }
 
 p <- ggplot(d, aes(x=to, y=from)) + theme_bw()
-p <- p + facet_grid(nattype ~ ., scales = "free_y", labeller=mf_labeller)
+p <- p + facet_grid(nattype ~ ., scales = "free_y", space = "free_y", labeller=mf_labeller)
 p <- p + geom_point(aes(size=requests, color=successrate))
 p <- p + scale_color_gradient2(low='red',mid="yellow", high="darkgreen", midpoint = 0.5)
 p <- p + labs(x = "Node Receiving", y = "Node Requesting\n")
 p <- p + opts(axis.text.x = theme_text(hjust=0, angle = -45))
+p <- p + opts(panel.margin = unit(0, "in"))
 p
 
 ggsave(file="nat.png", width=8, height=6, dpi=100)
