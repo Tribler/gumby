b7e8c45a2dd0c39bb9906321b3e4d61f0a85c67f
Niels Zeilemaker <niels@zeilemaker.nl>
2013-02-21 16:26:22 +0100

    Nat stuff r

--- das2_nat/nat.r
+++ das2_nat/nat.r
@@ -1,38 +1,33 @@
 library(ggplot2)
 library(reshape)
+library(stringr)
 
 r <- read.table("request.data", header=TRUE, row.names=1)
 s <- read.table("success.data", header=TRUE, row.names=1)
-
-colnames(r) <- 1:length(colnames(r))
-colnames(s) <- colnames(r)
-
-#resetting labels
-colnames(r) <- 1:length(colnames(r))
-colnames(s) <- 1:length(colnames(s))
-
-rownames(r) <- colnames(r)
-rownames(s) <- rownames(r)
-
 sr <- s/r
 
-r$From <- 1:length(rownames(r))
-sr$From <- 1:length(rownames(sr))
+r$nattype <- str_sub(rownames(r), -1)
+sr$nattype <- str_sub(rownames(r), -1)
 
-rm <- melt(r, id.vars = "From")
-srm <- melt(sr, id.vars = "From")
+r$from <- str_sub(rownames(r), 0, -3)
+sr$from <- str_sub(rownames(r), 0, -3)
 
-colnames(rm) <- c('From', 'To', 'Requests')
-colnames(srm) <- c('From', 'To', 'Successrate')
+rm <- melt(r, id.vars = c("from", "nattype"))
+srm <- melt(sr, id.vars = c("from", "nattype"))
+
+colnames(rm) <- c('from', 'nattype', 'to', 'requests')
+colnames(srm) <- c('from', 'nattype', 'to', 'successrate')
 
 d <- merge(rm, srm, sort=FALSE)
 d <- na.omit(d)
-midpoint <- 0.6
+midpoint <- 0.5
+d$to <- str_sub(d$to, 0, -3)
 
-p <- ggplot(d, aes(x=To, y=From)) + theme_bw() + geom_point(aes(size=Requests, color=Successrate))
+p <- ggplot(d, aes(x=to, y=from)) + theme_bw()
+p <- p + facet_grid(nattype ~ ., scales = "free_y")
+p <- p + geom_point(aes(size=requests, color=successrate))
 p <- p + scale_color_gradient2(low='red',mid="yellow", high="darkgreen", midpoint = midpoint)
-p <- p + scale_y_continuous(breaks=seq(1,length(colnames(r)),1))
-p <- p + labs(x = "Node Receiving", y = "Node Requesting") + opts(axis.text.x = theme_text(hjust = 0, colour = "grey50"), axis.text.y = theme_text(colour = "grey50"))
+p <- p + labs(x = "Node Receiving", y = "Node Requesting\n") + opts(axis.text.x = theme_text(hjust = 0, colour = "grey50", angle = -45), axis.text.y = theme_text(colour = "grey50"))
 p
 
 ggsave(file="nat.png", width=8, height=6, dpi=100)
