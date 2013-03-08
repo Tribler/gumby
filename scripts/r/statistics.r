843f730fae6a66882f7fc44b61604ddb7e556689
Niels Zeilemaker <niels@zeilemaker.nl>
2013-03-08 12:59:42 +0100

    Remove all variables ending with _ from the statistics graph

--- scripts/r/statistics.r
+++ scripts/r/statistics.r
@@ -7,6 +7,7 @@ maxX <- as.integer(commandArgs(TRUE)[2])
 if(file.exists("sum_statistics.txt")){
 	df <- read.table("sum_statistics.txt", header = TRUE, check.names = FALSE)
 	df <- melt(df, id="time")
+	df <- subset(df, str_sub(variable, -1) != '_')
 	
 	p <- ggplot(df, aes(time, value, group=variable, colour=variable)) + theme_bw()
 	p <- p + geom_line(data = df)
