d299916b5f6fe57b68bfbbf14f99b7e9ce05711e
Niels Zeilemaker <niels@zeilemaker.nl>
2013-03-15 13:32:53 +0100

    Tracking unique peers receiving a search message

--- private_search/search.r
+++ private_search/search.r
@@ -6,13 +6,13 @@ if(file.exists("searches.txt")){
     df <- read.table("searches.txt", header = TRUE, check.names = FALSE)
     df$ttl <- commandArgs(TRUE)[1]
     
-    df <- ddply(df, "ttl", summarise, mduration = mean(duration), mcycles = mean(nrcycles), mmessages = mean(nrmessages))
+    df <- ddply(df, "ttl", summarise, mduration = mean(duration), mcycles = mean(nrcycles), mmessages = mean(nrmessages), mumessages=mean(nruniquenodes))
     df <- melt(df, id="ttl")
     
     #remove duration for now
     df <- subset(df, variable != 'mduration')
     
-    facetLabels <- list('mcycles'="Mean #cycles occurred", 'mduration'="Mean duration", 'mmessages'="Mean #messages sent")
+    facetLabels <- list('mcycles'="Mean #cycles occurred", 'mduration'="Mean duration", 'mmessages'="Mean #messages sent", 'mumessages'="Mean #umessages sent")
     mf_labeller <- function(var, value){
         value <- as.character(value)
         return(facetLabels[value])
