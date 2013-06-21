8d76a4efa513cf4cae00a54d9d7a57113a4c51dd
Elric Milon <whirm@gmx.com>
2013-06-21 12:00:18 +0200

    Update the virtualenv building script to build boost and libtorrent with its python bindings

--- scripts/build_virtualenv.sh
+++ scripts/build_virtualenv.sh
@@ -6,16 +6,6 @@
 # Author: Elric Milon
 # Maintainer:
 # Created: Wed May 22 19:18:49 2013 (+0200)
-# Version:
-# Last-Updated:
-#           By:
-#     Update #: 16
-# URL:
-# Doc URL:
-# Keywords:
-# Compatibility:
-#
-#
 
 # Commentary:
 #
@@ -48,9 +38,19 @@
 # Code:
 set -ex
 
-VENV=$PWD/venv_tmp
+VENV=$PWD/venv
+
+if [ -e $VENV/.completed ]; then
+    echo "The virtualenv has been successfully built in a previous run of the script."
+    echo "If you want to rebuild it or the script has been updated, either delete $VENV/.completed"
+    echo "or the full $VENV dir and re-run the script."
+    exit 0
+fi
+
 
 virtualenv --no-site-packages --clear $VENV
+
+mkdir $VENV/src
 
 source $VENV/bin/activate
 
@@ -61,7 +61,7 @@ pushd build-tmp
 wget https://www.openssl.org/source/openssl-1.0.1e.tar.gz
 tar xvzpf openssl*tar.gz
 pushd openssl-*/
-ls
+
 ./config --prefix=$VENV threads zlib shared  --openssldir=$VENV/share/openssl
 #make -j$(grep processor /proc/cpuinfo | wc -l) #Fails when building in multithreaded mode
 make
@@ -103,6 +103,39 @@ popd
 #make -j$(grep processor /proc/cpuinfo | wc -l)
 #popd
 
+
+# Build libboost
+pushd $VENV/src
+if [ ! -e $VENV/lib/libboost_wserialization.so ]; then
+    wget http://netcologne.dl.sourceforge.net/project/boost/boost/1.53.0/boost_1_53_0.tar.bz2
+    tar xavf boost_*.tar.bz2
+    cd boost*/
+    ./bootstrap.sh
+    ./b2 -j$(grep process /proc/cpuinfo | wc -l) --prefix=$VENV install
+fi
+popd
+
+
+# Before continuing fix a stupid symlink bug
+#cd $VENV
+#rm lib64
+#ln -s lib lib64
+#cd
+
+# Build Libtorrent and its python bindings
+pushd $VENV/src
+if [ ! -e $VENV/lib/pkgconfig/libtorrent-rasterbar.pc ]; then
+    wget https://libtorrent.googlecode.com/files/libtorrent-rasterbar-0.16.10.tar.gz
+    tar xavf libtorrent-rasterbar-*.tar.gz
+    cd libtorrent-rasterbar*/
+    ./configure --with-boost-python --with-boost=$VENV/include/boost --with-boost-libdir=$VENV/lib --with-boost-system=boost_system --prefix=$VENV --enable-python-binding
+    make -j$(grep process /proc/cpuinfo | wc -l) || make
+    make install
+    cd $VENV/lib
+    ln -s libboost_python.so libboost_python-py27.so.1.53.0
+    cd ../..
+fi
+
 pip install -r requeriments.txt
 
 deactivate
@@ -110,12 +143,13 @@ deactivate
 virtualenv --relocatable $VENV
 
 unset LD_PRELOAD
-rm -fR venv
-mv $VENV $VENV/../venv
+#rm -fR venv
+#mv $VENV $VENV/../venv
 rm -fR build-tmp
+touch $VENV/.completed
 
-echo "Done, you can use this virtualenv with: 
-	source venv/bin/activate 
+echo "Done, you can use this virtualenv with:
+	source venv/bin/activate
 And exit from it with:
 	activate
 Enjoy."
