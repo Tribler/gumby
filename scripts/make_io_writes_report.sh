62cd400ba4c2e91d07c6d2fe80923ae0bba41aab
Elric Milon <whirm@gmx.com>
2013-07-08 14:19:03 +0200

    Try to find the proper paths while generating the reports

--- scripts/make_io_writes_report.sh
+++ scripts/make_io_writes_report.sh
@@ -10,8 +10,19 @@ fi
 OUTPUTDIR=../output/perf_reports
 
 mkdir -p $OUTPUTDIR/$1
-Rscript r/io_writes_report.R $1 $2 "$3"
 
-python make_io_writes_report.py $1
-cp ../templates/io_writes_report.css $OUTPUTDIR/$1/io_writes_report.css
+# Find the stp dir
+SCRIPTDIR=$( dirname $(readlink -f "$0"))
+if [ ! -d "$SCRIPTDIR" ]; then
+    SCRIPTDIR=$( dirname $(readlink -f $(which "$0")))
+fi
+if [ ! -d "$SCRIPTDIR" ]; then
+    echo "Couldn't find this script path, bailing out."
+    exit 1
+fi
+
+Rscript $SCRIPTDIR/r/io_writes_report.R $1 $2 "$3"
+
+python $SCRIPTDIR/make_io_writes_report.py $1
+cp $SCRIPTDIR/../templates/io_writes_report.css $OUTPUTDIR/$1/io_writes_report.css
 echo "Report created in $OUTPUTDIR/$1 ok"
