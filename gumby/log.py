ad37905ec9c6d4f98732db245339221259a7fcb6
Vlad Dumitrescu <vlad@dumitrescu.ro>
2013-07-23 12:43:56 +0200

    Header for log module

--- gumby/log.py
+++ gumby/log.py
@@ -1,3 +1,42 @@
+#!/usr/bin/env python
+# log.py ---
+#
+# Filename: log.py
+# Description:
+# Author: Vlad Dumitrescu
+# Maintainer:
+# Created: Tue Jul  23 12:14:19 2013 (+0200)
+
+# Commentary:
+# Logging helpers
+#
+#
+#
+
+# Change Log:
+#
+#
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+#
+#
+
+# Code:
+
 import inspect
 import logging
 from os.path import normcase
@@ -89,3 +128,6 @@ class GumbyPythonLoggingObserver(PythonLoggingObserver):
         logger = logging.getLogger(module)
 
         logger.log(level, text)
+
+#
+# log.py ends here
