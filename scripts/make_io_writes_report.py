9d31f82f3c703370bf745e44a864e49df4cc915e
Cor-Paul Bezemer <c.bezemer@tudelft.nl>
2013-07-30 15:25:46 +0200

    fix csv unicode support

--- scripts/make_io_writes_report.py
+++ scripts/make_io_writes_report.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys
 import os
 from jinja2 import Environment, FileSystemLoader
-import csv
+import unicodecsv
+import codecs
 
-#THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+# THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 def setReportName():
     global THIS_DIR
@@ -14,37 +18,41 @@ def setReportName():
         print 'usage python make_io_writes_report.py output_path'
         exit()
 
+
 def readSummary():
     with file(THIS_DIR + '/summary.txt') as f:
         s = f.read()
     return s
 
+
 def readDataframeDump(filename):
     var = []
     with open(filename, 'rb') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter = ',')
+        reader = unicodecsv.DictReader(csvfile, delimiter=',')
+        # reader = UnicodeDictReader(csvfile)
         for line in reader:
             var.append(line)
     return var
 
+
 def print_html_doc():
     template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../templates'))
     print "Getting templates from: %s" % template_dir
-    loader = FileSystemLoader(searchpath = template_dir)
+    loader = FileSystemLoader(searchpath=template_dir)
     env = Environment(loader=loader)
     template = env.get_template('template_io_writes_report.html')
+    top20perSt = readDataframeDump(THIS_DIR + '/top20_per_stacktrace.csv')
     report = template.render(
-            title= 'IO Writes Report',
-            summary = readSummary(),
-            top20PerStacktrace = readDataframeDump(THIS_DIR + '/top20_per_stacktrace.csv'),
-            top20PerFilename = readDataframeDump(THIS_DIR + '/top20_per_filename.csv'),
-            topLargestWrites = readDataframeDump(THIS_DIR + '/top_largest_writes.csv')
+            title='IO Writes Report',
+            summary=readSummary(),
+            top20PerStacktrace=top20perSt,
+            top20PerFilename=readDataframeDump(THIS_DIR + '/top20_per_filename.csv'),
+            topLargestWrites=readDataframeDump(THIS_DIR + '/top_largest_writes.csv')
 
 
-            )
+            ).encode("utf-8")
     with open(THIS_DIR + '/io_writes_report.html', 'wb') as fh:
         fh.write(report)
-
 
 if __name__ == '__main__':
     setReportName()
