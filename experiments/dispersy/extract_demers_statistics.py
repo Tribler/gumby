adab704edf821b7bb29a41cd62740eab8f7e726a
Elric Milon <whirm@gmx.com>
2013-10-23 18:44:06 +0200

    convert to unix newlines, add header and shebang

--- experiments/dispersy/extract_demers_statistics.py
+++ experiments/dispersy/extract_demers_statistics.py
@@ -1,7 +1,49 @@
+#!/usr/bin/env python
+# extract_demers_statistics.py ---
+#
+# Filename: extract_demers_statistics.py
+# Description:
+# Author: Elric Milon
+# Maintainer:
+# Created: Wed Oct 23 18:41:17 2013 (+0200)
+
+# Commentary:
+#
+#
+#
+#
+
+# Change Log:
+#
+#
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+#
+#
+
+# Code:
+
+
 from itertools import groupby
 from extract_dispersy_statistics import *
 
+
 class DemersMessages(AbstractHandler):
+
     def __init__(self):
         AbstractHandler.__init__(self)
 
@@ -46,3 +88,6 @@ if __name__ == "__main__":
     e = get_parser(sys.argv)
     e.add_handler(DemersMessages())
     e.parse()
+
+#
+# extract_demers_statistics.py ends here
