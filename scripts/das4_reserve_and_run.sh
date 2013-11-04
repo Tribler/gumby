25377d926e7ee29fb2ac9897bbc9708a21ce090b
Niels Zeilemaker <niels@zeilemaker.nl>
2013-11-04 18:18:50 +0100

    Merged some functionality from das4-start. Added call to qdel, fixing dr problems as reported by Bogdan

--- scripts/das4_reserve_and_run.sh
+++ scripts/das4_reserve_and_run.sh
@@ -52,11 +52,33 @@ fi
 
 echo "Reserving $DAS4_NODE_AMOUNT nodes for $DAS4_RESERVE_DURATION secs."
 
-prun -t $DAS4_RESERVE_DURATION -v -np $DAS4_NODE_AMOUNT das4_node_run_job.sh &
+JOB_STATUS_FILE=/tmp/das4_job_$USER
+export RMS_DEBUG=1
+(prun -t $DAS4_RESERVE_DURATION -v -np $DAS4_NODE_AMOUNT das4_node_run_job.sh | tee $JOB_STATUS_FILE ) &
 PID=$!
 
-sleep 1
+sleep 2
+
+RESERVATION_ID=$(head -n1 $JOB_STATUS_FILE | sed 's/Reservation number \([0-9]*\):.*/\1/g')
+rm $JOB_STATUS_FILE
+
+echo
 preserve -llist
+echo
+echo "*** If the job gets queued, you can cancel waiting using Ctrl+C"
+echo "*** (check DAS4 status with 'preserve -llist')"
+echo
+
+
+function stop_everything {
+    kill -1 $PID &>/dev/null # according to http://www.cs.vu.nl/das4/prun.shtml, must send SIGINT
+    qdel $RESERVATION_ID
+    exit
+}
+trap stop_everything SIGHUP SIGTERM SIGINT
+
 wait $PID
+qdel $RESERVATION_ID
+
 #
 # das4_reserve_and_run.sh ends here
