404d723676ca8e884b5c28de47d5c2eea8184e96
Niels Zeilemaker <niels@zeilemaker.nl>
2013-11-04 16:48:02 +0100

    Removed dependency on qplot reshape2 and some other obscure r packages.

--- scripts/r/install.r
+++ scripts/r/install.r
@@ -1,6 +1,6 @@
 is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
 
-toInstall <- c("ggplot2", "reshape", "gplots", "plotrix","stringr","grid", "gridExtra", "plyr")
+toInstall <- c("ggplot2", "reshape", "stringr", "plyr")
 for (package in toInstall){
 	if (is.installed(package) == FALSE){
 		install.packages(package, repos = "http://cran.r-project.org")
