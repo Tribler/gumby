58218b46c2b598387044f0965442cd8cad8e1f40
Niels Zeilemaker <niels@zeilemaker.nl>
2013-12-02 20:47:04 +0100

    Moved logger

--- experiments/dispersy/privatesearch_client.py
+++ experiments/dispersy/privatesearch_client.py
@@ -186,10 +186,12 @@ class PrivateSearchClient(DispersyExperimentScriptClient):
         DispersyExperimentScriptClient.start_dispersy(self)
         self.community_args = (self._my_member,)
 
-    @call_on_dispersy_thread
-    def connect_to_taste_buddies(self):
+    def online(self):
+        DispersyExperimentScriptClient.online(self)
         self._dispersy.callback.persistent_register(u"log_statistics", self.log_statistics)
 
+    @call_on_dispersy_thread
+    def connect_to_taste_buddies(self):
         if not self.late_joining:
             nr_to_connect = int(10 * self.bootstrap_percentage)
             if self.random_connect:
@@ -203,7 +205,6 @@ class PrivateSearchClient(DispersyExperimentScriptClient):
 
     @call_on_dispersy_thread
     def perform_searches(self):
-        self._dispersy.callback.persistent_register(u"log_statistics", self.log_statistics)
         while True:
             self.nr_search += 1
 
