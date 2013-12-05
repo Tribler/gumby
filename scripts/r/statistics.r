24936fa70ba62f07f4890e9e5a800e80f9b09943
Niels Zeilemaker <niels@zeilemaker.nl>
2013-12-05 17:08:00 +0100

    Typos

--- scripts/r/statistics.r
+++ scripts/r/statistics.r
@@ -7,10 +7,9 @@ if(file.exists("sum_statistics_reduced.txt")){
 	df <- df[,colSums(df) != 0]
 	df <- melt(df, id="time")
 	df <- subset(df, str_sub(variable, -1) != '_')
-	p <- p + geom_point(data = df2)
+	df2 <- df[!duplicated(df[,2:3]),]
 	
-	
-	p <- ggplot(df2, aes(time, value, group=variable, colour=variable, shape=variable)) + theme_bw()
+	p <- ggplot(df, aes(time, value, group=variable, colour=variable, shape=variable)) + theme_bw()
 	p <- p + geom_step()
 	p <- p + geom_point(data = df2)
 	p <- p + theme(legend.position="bottom", legend.direction="horizontal")
