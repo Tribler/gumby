311a174823421935ebcf897d766a097dcf022d6c
Niels Zeilemaker <niels@zeilemaker.nl>
2014-01-07 12:43:55 +0100

    Moved annotation stuff to separate file

---
+++ scripts/r/annotation.r
@@ -0,0 +1,20 @@
+load_annotations <- function(){
+	if(file.exists("annotations.txt")){
+		df2 <- read.table("annotations.txt", header = TRUE, check.names = FALSE)
+		show_mean <- length(colnames(df2)) != 3
+		df2 <- melt(df2)
+		df2 <- ddply(df2, .(annotation), summarise, meanx = mean(value), minx = min(value), maxx = max(value))
+		df2$linesize <- max((df2$maxx - df2$minx) / 2, 1)
+		df2$linepos <- df2$minx + df2$linesize
+	}
+}
+
+add_annotations <- function(){
+	if(file.exists("annotations.txt")){
+		p <- p + stat_vline(alpha = 0.2, data=df2, xintercept = df2$linepos, size = df2$linesize, mapping = aes(colour=annotation))
+		if (show_mean) {
+			p <- p + stat_vline(alpha = 0.6, data=df2, xintercept = df2$meanx, size = 1, mapping = aes(colour=annotation))
+		}
+		p <- p + geom_text(alpha = 0.4, data=df2, angle = 90, aes(x=maxx, y=max(df$value), label=annotation, hjust=1, size=6))
+	}
+}
--- scripts/r/connections.r
+++ scripts/r/connections.r
@@ -4,14 +4,8 @@ library(reshape)
 minX <- as.integer(commandArgs(TRUE)[1])
 maxX <- as.integer(commandArgs(TRUE)[2])
 
-if(file.exists("annotations.txt")){
-	df2 <- read.table("annotations.txt", header = TRUE, check.names = FALSE)
-	show_mean <- length(colnames(df2)) != 3
-	df2 <- melt(df2)
-	df2 <- ddply(df2, .(annotation), summarise, meanx = mean(value), minx = min(value), maxx = max(value))
-	df2$linesize <- max((df2$maxx - df2$minx) / 2, 1)
-	df2$linepos <- df2$minx + df2$linesize
-}
+source('annotation.r')
+load_annotations()
 
 i = 1
 while(file.exists(paste("total_connections_", toString(i), "_reduced.txt", sep = ''))){
@@ -20,13 +14,7 @@ while(file.exists(paste("total_connections_", toString(i), "_reduced.txt", sep =
 	
 	p <- ggplot(df) + theme_bw()
 	
-	if(file.exists("annotations.txt")){
-		p <- p + stat_vline(alpha = 0.2, data=df2, xintercept = df2$linepos, size = df2$linesize, mapping = aes(colour=annotation))
-		p <- p + geom_text(alpha = 0.4, data=df2, angle = 90, aes(x=maxx, y=max(df$value), label=annotation, hjust=1, size=6))
-		if (show_mean) {
-			p <- p + stat_vline(alpha = 0.6, data=df2, xintercept = df2$meanx, size = 1, mapping = aes(colour=annotation))
-		}
-	}
+	add_annotations()
 	
 	p <- p + geom_step(data = df, alpha = 0.8, aes(time, value, group=variable, colour=variable))
 	p <- p + theme(legend.position = "none")
@@ -46,13 +34,7 @@ while(file.exists(paste("sum_incomming_connections_", toString(i), "_reduced.txt
 	
 	p <- ggplot(df, aes(x=value)) + theme_bw()
 	
-	if(file.exists("annotations.txt")){
-		p <- p + stat_vline(alpha = 0.2, data=df2, xintercept = df2$linepos, size = df2$linesize, mapping = aes(colour=annotation))
-		p <- p + geom_text(alpha = 0.4, data=df2, angle = 90, aes(x=maxx, y=max(df$value), label=annotation, hjust=1, size=6))
-		if (show_mean) {
-			p <- p + stat_vline(alpha = 0.6, data=df2, xintercept = df2$meanx, size = 1, mapping = aes(colour=annotation))
-		}
-	}
+	add_annotations()
 	
 	p <- p + geom_density()
 	p <- p + geom_histogram(aes(y=..density.., alpha=0.8))
@@ -72,13 +54,7 @@ while(file.exists(paste("bl_skip_", toString(i), "_reduced.txt", sep = ''))){
     
     p <- ggplot(df, aes(time, value, group=variable, colour=variable)) + theme_bw()
 	
-	if(file.exists("annotations.txt")){
-		p <- p + stat_vline(alpha = 0.2, data=df2, xintercept = df2$linepos, size = df2$linesize, mapping = aes(colour=annotation))
-		p <- p + geom_text(alpha = 0.4, data=df2, angle = 90, aes(x=maxx, y=max(df$value), label=annotation, hjust=1, size=6))
-		if (show_mean) {
-			p <- p + stat_vline(alpha = 0.6, data=df2, xintercept = df2$meanx, size = 1, mapping = aes(colour=annotation))
-		}
-	}
+	add_annotations()
 	
     p <- p + geom_step(data = df, alpha=0.8)
 	p <- p + theme(legend.position = "none")
@@ -98,13 +74,7 @@ while(file.exists(paste("bl_reuse_", toString(i), "_reduced.txt", sep = ''))){
     
     p <- ggplot(df, aes(time, value, group=variable, colour=variable)) + theme_bw()
 	
-	if(file.exists("annotations.txt")){
-		p <- p + stat_vline(alpha = 0.2, data=df2, xintercept = df2$linepos, size = df2$linesize, mapping = aes(colour=annotation))
-		p <- p + geom_text(alpha = 0.4, data=df2, angle = 90, aes(x=maxx, y=max(df$value), label=annotation, hjust=1, size=6))
-		if (show_mean) {
-			p <- p + stat_vline(alpha = 0.6, data=df2, xintercept = df2$meanx, size = 1, mapping = aes(colour=annotation))
-		}
-	}
+	add_annotations()
 	
     p <- p + geom_step(data = df, alpha=0.8)
 	p <- p + theme(legend.position = "none")
@@ -124,13 +94,7 @@ while(file.exists(paste("bl_time_", toString(i), "_reduced.txt", sep = ''))){
 	
 	p <- ggplot(df, aes(time, value, group=variable, colour=variable)) + theme_bw()
 	
-	if(file.exists("annotations.txt")){
-		p <- p + stat_vline(alpha = 0.2, data=df2, xintercept = df2$linepos, size = df2$linesize, mapping = aes(colour=annotation))
-		p <- p + geom_text(alpha = 0.4, data=df2, angle = 90, aes(x=maxx, y=max(df$value), label=annotation, hjust=1, size=6))
-		if (show_mean) {
-			p <- p + stat_vline(alpha = 0.6, data=df2, xintercept = df2$meanx, size = 1, mapping = aes(colour=annotation))
-		}
-	}
+	add_annotations()
 	
 	p <- p + geom_step(data = df, alpha=0.8)
 	p <- p + theme(legend.position = "none")
