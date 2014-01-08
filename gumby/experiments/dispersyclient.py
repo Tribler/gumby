eb6d3ab5f3ad60023158558526951e415fe947f9
Niels Zeilemaker <niels@zeilemaker.nl>
2014-01-08 13:40:11 +0100

    Lambda was passed an argument

--- gumby/experiments/dispersyclient.py
+++ gumby/experiments/dispersyclient.py
@@ -485,8 +485,8 @@ def main(client_class):
     factory = ExperimentClientFactory({}, client_class)
     msg("Connecting to: %s:%s" % (environ['SYNC_HOST'], int(environ['SYNC_PORT'])))
     # Wait for a random amount of time before connecting to try to not overload the server when we have a lot of connections
-    reactor.callLater(random()*10,
-                      lambda _: reactor.connectTCP(environ['SYNC_HOST'], int(environ['SYNC_PORT']), factory))
+    reactor.callLater(random() * 10,
+                      lambda: reactor.connectTCP(environ['SYNC_HOST'], int(environ['SYNC_PORT']), factory))
     reactor.exitCode = 0
     reactor.run()
     exit(reactor.exitCode)
