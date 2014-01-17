72d6b089d881074540eaa43017bdf45e5799216c
Niels Zeilemaker <niels@zeilemaker.nl>
2014-01-17 17:49:03 +0100

    Use mean_max_min for diff graphs

--- scripts/r/drop.r
+++ scripts/r/drop.r
@@ -9,12 +9,23 @@ df2 <- load_annotations()
 
 if(file.exists("dropped_diff_reduced.txt")){
 	df <- read.table("dropped_diff_reduced.txt", header = TRUE)
-	df <- melt(df, id="time")
-	df <- subset(df, df$value > 0)
+	num_columns <- ncol(df) - 1
+	df <- mean_max_min(num_columns, df)
+	df$value = df$value/1024.0
+	
+	if (num_columns <= 1000){
+		df <- subset(df, df$value > 0)
+	}
 	
 	p <- ggplot(df) + theme_bw()
 	p <- add_annotations(p, df2)
-	p <- p + geom_point(alpha = 0.8, aes(time, value, size=value, group=variable, colour=variable)) + scale_size(range = c(1, 3))
+	if (num_columns <= 1000){
+		p <- p + geom_point(alpha = 0.8, aes(time, value, size=value, group=variable, colour=variable)) + scale_size(range = c(1, 3))
+	} else {
+		p <- p + geom_line(aes(time, mean), colour = '2')
+		p <- p + geom_ribbon(alpha = 0.3, aes(time, mean, ymin=min, ymax=max, linetype=NA))
+		p <- p + geom_ribbon(alpha = 0.3, aes(time, ymin=Q1, ymax=Q3, linetype=NA))
+	}
 	p <- p + theme(legend.position = "none")
 	p <- p + labs(x = "\nTime into experiment (Seconds)", y = "Messages dropped (Diff)\n")
 	p <- p + xlim(minX, maxX)
--- scripts/r/send_diff.r
+++ scripts/r/send_diff.r
@@ -9,13 +9,23 @@ df2 <- load_annotations()
 
 if(file.exists("send_diff_reduced.txt")){
 	df <- read.table("send_diff_reduced.txt", header = TRUE, check.names = FALSE)
-	df <- melt(df, id="time")
-	df <- subset(df, df$value > 0)
+	num_columns <- ncol(df) - 1
+	df <- mean_max_min(num_columns, df)
 	df$value = df$value/1024.0
+	
+	if (num_columns <= 1000){
+		df <- subset(df, df$value > 0)
+	}
 	
 	p <- ggplot(df) + theme_bw()
 	p <- add_annotations(p, df2)
-	p <- p + geom_point(data = df, aes(time, value, group=variable, colour=variable, size=value), alpha=0.8)
+	if (num_columns <= 1000){
+		p <- p + geom_point(data = df, aes(time, value, group=variable, colour=variable, size=value), alpha=0.8)  + scale_size(range = c(1, 3))
+	} else {
+		p <- p + geom_line(aes(time, mean), colour = '2')
+		p <- p + geom_ribbon(alpha = 0.3, aes(time, mean, ymin=min, ymax=max, linetype=NA))
+		p <- p + geom_ribbon(alpha = 0.3, aes(time, ymin=Q1, ymax=Q3, linetype=NA))
+	}
 	p <- p + theme(legend.position = "none")
 	p <- p + labs(x = "\nTime into experiment (Seconds)", y = "Bandwidth usage for peer (KiBytes/s upload)\n")
 	p <- p + xlim(minX, maxX)
@@ -25,13 +35,23 @@ if(file.exists("send_diff_reduced.txt")){
 
 if(file.exists("received_diff_reduced.txt")){
 	df <- read.table("received_diff_reduced.txt", header = TRUE, check.names = FALSE)
-	df <- melt(df, id="time")
-	df <- subset(df, df$value > 0)
+	num_columns <- ncol(df) - 1
+	df <- mean_max_min(num_columns, df)
 	df$value = df$value/1024.0
+	
+	if (num_columns <= 1000){
+		df <- subset(df, df$value > 0)
+	}
 	
 	p <- ggplot(df) + theme_bw()
 	p <- add_annotations(p, df2)
-	p <- p + geom_point(data = df, aes(time, value, group=variable, colour=variable, size=value), alpha=0.8)
+	if (num_columns <= 1000){
+		p <- p + geom_point(data = df, aes(time, value, group=variable, colour=variable, size=value), alpha=0.8)  + scale_size(range = c(1, 3))
+	} else {
+		p <- p + geom_line(aes(time, mean), colour = '2')
+		p <- p + geom_ribbon(alpha = 0.3, aes(time, mean, ymin=min, ymax=max, linetype=NA))
+		p <- p + geom_ribbon(alpha = 0.3, aes(time, ymin=Q1, ymax=Q3, linetype=NA))
+	}
 	p <- p + theme(legend.position = "none")
 	p <- p + labs(x = "\nTime into experiment (Seconds)", y = "Bandwidth usage for peer (KiBytes/s download)\n")
 	p <- p + xlim(minX, maxX)
