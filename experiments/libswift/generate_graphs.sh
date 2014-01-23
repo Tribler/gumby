7cc3a304a93b98fad5101a0415f0ff04afab89e4
Riccardo <r.petrocco@gmail.com>
2014-01-23 17:39:37 +0100

    added initial files for graph generation

--- experiments/libswift/generate_graphs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-# TODO(emilon): Maybe move this to the general setup script
-#make sure the R local install dir exists
-mkdir -p $R_LIBS_USER
-R --no-save --quiet < $R_SCRIPTS_PATH/install.r
-graph_process_guard_data.sh
-
-
