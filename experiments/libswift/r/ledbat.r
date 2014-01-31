367e757f629c9cad2e79813b83be4130bc0bc90e
Riccardo <r.petrocco@gmail.com>
2014-01-31 15:21:18 +0100

    removed ledbat for leecher

--- experiments/libswift/r/ledbat.r
+++ experiments/libswift/r/ledbat.r
@@ -1,17 +1,14 @@
 library(ggplot2)
 library(reshape)
 
-ledbat_files <- list.files(pattern="*.cc")
-for (k in 1:length(ledbat_files)) {
-    df <- read.table(ledbat_files[k], header = TRUE, check.names = FALSE,  fill = TRUE)
-
-    output <- unlist(strsplit(ledbat_files[k], "\\."))
+if (file.exists("seeder.cc")) {
+    df <- read.table("seeder.cc", header = TRUE, check.names = FALSE,  fill = TRUE)
 
     p <- ggplot(df, aes(x=time, y=window)) + geom_line()
     p <- p + theme(legend.position="top")
-    p <- p + labs(x = "\nTime (Seconds)", y = "Window size\n", title = paste(output[1], "cc window"))
+    p <- p + labs(x = "\nTime (Seconds)", y = "Window size\n", title="Ledbat window")
     p
 
-    ggsave(file=paste(output[1], "_cc.png"), width=8, height=6, dpi=100)
+    ggsave(file="seeder_cc.png", width=8, height=6, dpi=100)
 }
 
