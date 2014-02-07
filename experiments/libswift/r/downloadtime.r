750f2c2c32ab9732d42c13c87f4ca962b1b789cc
badrock <r.petrocco@gmail.com>
2014-02-07 15:43:59 +0100

    added complete graph

--- experiments/libswift/r/downloadtime.r
+++ experiments/libswift/r/downloadtime.r
@@ -15,6 +15,24 @@ for (k in 1:length(stderr_files)) {
     p <- p + labs(x = "\nTime (Seconds)", y = "Speed (KByte/s)\n", title = paste(output[1], "network speeds"))
     p
 
-    ggsave(file=paste(output[1], ".png"), width=8, height=6, dpi=100)
+    ggsave(file=paste(output[1], "speed.png", sep = "_"), width=8, height=6, dpi=100)
+
 }
 
+for (k in 1:length(stderr_files)) {
+ 
+    df <- read.table(stderr_files[k], header = TRUE, check.names = FALSE)
+    df$upload <- NULL
+    df$download <- NULL
+
+    output <- unlist(strsplit(stderr_files[k], "\\."))
+
+    p <- ggplot(df, aes(x=time, y=percent))
+    p <- p + geom_line() + ylim(0,100)
+    p <- p + theme(legend.position="top", legend.title=element_blank())
+    p <- p + labs(x = "\nTime (Seconds)", y = "Completion (Percent %)\n", title = paste(output[1], "Completion"))
+    p
+
+    ggsave(file=paste(output[1], "complete.png", sep = "_"), width=8, height=6, dpi=100)
+
+}
