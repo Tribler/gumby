ce720c91ccebda6685227448d1fe4e42c1fc62a6
Niels Zeilemaker <niels@zeilemaker.nl>
2014-04-22 15:08:15 +0200

    Assert to check if master_member and my_member are intialized

--- gumby/experiments/dispersyclient.py
+++ gumby/experiments/dispersyclient.py
@@ -246,6 +246,8 @@ class DispersyExperimentScriptClient(ExperimentClient):
 
         self._master_member = self._dispersy.callback.call(self._dispersy.get_member, kargs={'private_key': self.master_private_key})
         self._my_member = self._dispersy.callback.call(self._dispersy.get_member, kargs={'private_key': self.my_member_private_key})
+        assert self._master_member
+        assert self._my_member
 
         self._dispersy.callback.register(self._do_log)
 
