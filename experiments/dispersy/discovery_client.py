03397befccb0d42cad89a217beaab535acaa4c55
Egbert Bouman <ebouman@gmail.com>
2014-05-12 15:36:31 +0200

    Use LoopingCall for monitor_friends

--- experiments/dispersy/discovery_client.py
+++ experiments/dispersy/discovery_client.py
@@ -10,10 +10,11 @@ from hashlib import sha1
 from gumby.experiments.dispersyclient import DispersyExperimentScriptClient, main, buffer_online
 
 from twisted.python.log import msg
+from twisted.python.threadable import isInIOThread
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet import reactor
+from twisted.internet.task import LoopingCall
 from twisted.internet.threads import deferToThread
-
 
 # TODO(emilon): Fix this crap
 pythonpath.append(path.abspath(path.join(path.dirname(__file__), '..', '..', '..', "./tribler")))
@@ -35,7 +36,9 @@ class DiscoveryClient(DispersyExperimentScriptClient):
         self.set_community_kwarg('max_prefs', 10)
         self.set_community_kwarg('max_tbs', 25)
 
-        self.monitor_friends_deferred = None
+        self.monitor_friends_lc = None
+        self._prev_scenario_statistics = {}
+        self._prev_scenario_debug = {}
 
     def registerCallbacks(self):
         self.scenario_runner.register(self.insert_my_key, 'insert_my_key')
@@ -68,31 +71,40 @@ class DiscoveryClient(DispersyExperimentScriptClient):
         pass
 
     def add_friend(self, peer_id):
-        if peer_id != self.my_id:
-            peer_id = int(peer_id)
+        try:
+            if peer_id != self.my_id:
+                peer_id = int(peer_id)
 
-            # if we don't get the ipport, then this peer isn't deployed to the das
-            ipport = self.get_peer_ip_port_by_id(peer_id)
-            key = self.get_private_keypair_by_id(peer_id)
+                # if we don't get the ipport, then this peer isn't deployed to the das
+                ipport = self.get_peer_ip_port_by_id(peer_id)
+                key = self.get_private_keypair_by_id(peer_id)
 
-            if ipport and key:
-                key = key.pub()
-                keyhash = self._crypto.key_to_hash(key)
-                keyhash_as_long = long(sha1(self._crypto.key_to_bin(key)).hexdigest(), 16)
+                print >> sys.stderr, "add_friend", peer_id, ipport, bool(key)
 
-                self.preferences.add(sha1(str(peer_id)).digest())
+                if ipport and key:
+                    key = key.pub()
+                    keyhash = self._crypto.key_to_hash(key)
+                    keyhash_as_long = long(sha1(self._crypto.key_to_bin(key)).hexdigest(), 16)
 
-                self.friends.add(keyhash)
-                self.friendhashes[peer_id] = keyhash_as_long
-                self.friendiphashes[ipport] = keyhash_as_long
+                    self.preferences.add(sha1(str(peer_id)).digest())
 
-                if self.monitor_friends_deferred == None:
-                    self.monitor_friends_deferred = self.monitor_friends()
+                    self.friends.add(keyhash)
+                    self.friendhashes[peer_id] = keyhash_as_long
+                    self.friendiphashes[ipport] = keyhash_as_long
 
-            elif ipport:
-                print >> sys.stderr, "Got ip/port, but not key?", peer_id
-            else:
-                print >> sys.stderr, "No ip/port or key", peer_id
+                    print >> sys.stderr, "friend added", len(self.friends)
+
+                    if not self.monitor_friends_lc:
+                        self.monitor_friends_lc = lc = LoopingCall(self.monitor_friends)
+                        lc.start(5.0, now=True)
+
+                elif ipport:
+                    print >> sys.stderr, "Got ip/port, but not key?", peer_id
+                else:
+                    print >> sys.stderr, "No ip/port or key", peer_id
+        except:
+            from traceback import print_exc
+            print_exc()
 
     @buffer_online
     def add_foaf(self, peer_id, his_friends):
@@ -105,27 +117,20 @@ class DiscoveryClient(DispersyExperimentScriptClient):
     def connect_to_friends(self):
         pass
 
-    @inlineCallbacks
     def monitor_friends(self):
-        prev_scenario_statistics = {}
-        prev_scenario_debug = {}
+        connected_friends = 0
+        for mid in self.friends:
+            if self._community and self._community.is_taste_buddy_mid(mid):
+                connected_friends += 1
 
-        while True:
-            connected_friends = 0
-            for mid in self.friends:
-                if self._community and self._community.is_taste_buddy_mid(mid):
-                    connected_friends += 1
+        if self.friends:
+            bootstrapped = connected_friends / float(len(self.friends))
+        else:
+            bootstrapped = 0
 
-            if self.friends:
-                bootstrapped = connected_friends / float(len(self.friends))
-            else:
-                bootstrapped = 0
-
-            prev_scenario_statistics = self.print_on_change("scenario-statistics", prev_scenario_statistics, {'bootstrapped': bootstrapped})
-            if self._community:
-                prev_scenario_debug = self.print_on_change("scenario-debug", prev_scenario_debug, {'nr_friends':len(self.friends) if self.friends else 0})
-
-            yield deferLater(reactor, 5.0, lambda : None)
+        self._prev_scenario_statistics = self.print_on_change("scenario-statistics", self._prev_scenario_statistics, {'bootstrapped': bootstrapped})
+        if self._community:
+            self._prev_scenario_debug = self.print_on_change("scenario-debug", self._prev_scenario_debug, {'nr_friends':len(self.friends) if self.friends else 0})
 
 if __name__ == '__main__':
     DiscoveryClient.scenario_file = environ.get('SCENARIO_FILE', 'social.scenario')
