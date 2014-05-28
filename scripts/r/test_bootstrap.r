7cd1021034b5faf43ed37154fe8745cd0fe970ed
Niels Zeilemaker <niels@zeilemaker.nl>
2014-05-28 11:40:39 +0200

    Fixup

--- scripts/r/test_bootstrap.r
+++ scripts/r/test_bootstrap.r
@@ -3,7 +3,7 @@ library(reshape)
 
 walk_rtts <- read.table("walk_rtts.txt", header=T, quote="\"")
 walk_rtts$label <- factor(paste(walk_rtts$HOST_NAME, "(", walk_rtts$ADDRESS, ")", sep=''))
-p <- ggplot(walk_rtts, aes(label, RTT))
+p <- ggplot(walk_rtts, aes(HOST_NAME, RTT))
 p <- p + geom_boxplot(aes(fill=label))
 p <- p + coord_flip()
 p <- p + labs(title="Bootstrap server response time", 
@@ -15,7 +15,7 @@ ggsave("walk_rtts.png", width=10, height=6, dpi=100)
 
 summary <- read.table("summary.txt", header=T, quote="\"")
 summary$label <- factor(paste(summary$HOST_NAME, "(", summary$ADDRESS, ")", sep=''))
-p <- ggplot(summary, aes(label, RESPONSES))
+p <- ggplot(summary, aes(HOST_NAME, RESPONSES))
 p <- p + geom_bar(aes(fill=label))
 p <- p + coord_flip()
 p <- p + ylim(0, max(summary$REQUESTS))
