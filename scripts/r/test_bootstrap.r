d2dd5f58eed0c0407ea24976c9017eb8dff276dc
Niels Zeilemaker <niels@zeilemaker.nl>
2014-05-28 18:49:50 +0200

    Fix sorting

--- scripts/r/test_bootstrap.r
+++ scripts/r/test_bootstrap.r
@@ -3,10 +3,11 @@ library(reshape)
 
 walk_rtts <- read.table("walk_rtts.txt", header=T, quote="\"")
 walk_rtts$label <- factor(paste(walk_rtts$HOST_NAME, "(", walk_rtts$ADDRESS, ")", sep=''))
+
 p <- ggplot(walk_rtts, aes(HOST_NAME, RTT))
 p <- p + geom_boxplot(aes(fill=label))
 p <- p + coord_flip()
-p <- p + scale_x_discrete(limits=rev(factor(walk_rtts$HOST_NAME)))
+p <- p + scale_x_discrete(limits=rev(sort(summary$HOST_NAME)))
 p <- p + labs(title="Bootstrap server response time", 
               x="Server address", 
               y="Round-trip time (seconds)",
@@ -16,16 +17,19 @@ ggsave("walk_rtts.png", width=10, height=6, dpi=100)
 
 summary <- read.table("summary.txt", header=T, quote="\"")
 summary$label <- factor(paste(summary$HOST_NAME, "(", summary$ADDRESS, ")", sep=''))
+
 p <- ggplot(summary, aes(HOST_NAME, RESPONSES))
 p <- p + geom_bar(aes(fill=label))
 p <- p + coord_flip()
 p <- p + ylim(0, max(summary$REQUESTS))
-p <- p + scale_x_discrete(limits=rev(factor(summary$HOST_NAME)))
+p <- p + scale_x_discrete(limits=rev(sort(summary$HOST_NAME)))
 p <- p + labs(title=paste("Bootstrap server walk request success\nout of", max(summary$REQUESTS), "requests"),
               x="Server address",
               y="Successfull walks",
               colour="Server hostname")
 p
+
+
 ggsave("summary.png", width=10, height=6, dpi=100)
 
 q(save="no")
