0e075634d6e668ee77e330bb6a734b4d6fb05877
Elric Milon <whirm@gmx.com>
2014-07-16 15:46:04 +0200

    Always use lib/passwd as the passwd file

--- gumby/instrumentation.py
+++ gumby/instrumentation.py
@@ -92,12 +92,13 @@ def start_manhole():
     """
     Starts a manhole telnet server listening on MANHOLE_PORT
     """
+    passwd_path = os.path.join(environ['PROJECT_DIR'], 'lib', 'passwd')
     global manhole
     manhole = manhole_tap.makeService({
         'namespace': manhole_namespace,
         'telnetPort': 'tcp:%d:interface:127.0.0.1' % MANHOLE_PORT,
         'sshPort': None,
-        'passwd': 'passwd',
+        'passwd': passwd_path,
     })
 
 
