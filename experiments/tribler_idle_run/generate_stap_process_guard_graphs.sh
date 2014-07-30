ac09c5a50dbc8349a2317777a36425cc27c6987c
CP <c.bezemer@tudelft.nl>
2014-07-30 14:57:33 +0200

    small optimization

--- experiments/tribler_idle_run/generate_stap_process_guard_graphs.sh
+++ experiments/tribler_idle_run/generate_stap_process_guard_graphs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-stap_make_io_writes_report.sh $OUTPUT_DIR/report $OUTPUT_DIR/stap.csv "1H run report" 
-graph_process_guard_data.sh
+stap_make_io_writes_report.sh $OUTPUT_DIR/report $OUTPUT_DIR/stap.csv "1H run report" &
+graph_process_guard_data.sh &
+wait
