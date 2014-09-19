ab5e9bc6a13b0ea2a056c67a377ae0960cb32bb8
Egbert Bouman <ebouman@gmail.com>
2014-09-19 14:20:35 +0200

    Use different endpoint for tunnel experiment

--- experiments/dispersy/tunnel_client.py
+++ experiments/dispersy/tunnel_client.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 
-import sys
-from os import path, environ
-from time import time
-from random import sample, randint
+from os import path, getpid
+from random import choice
+from string import letters
 from sys import path as pythonpath
-from hashlib import sha1
+from time import time
 
-from gumby.experiments.dispersyclient import DispersyExperimentScriptClient, main, buffer_online
+from threading import Event
 
-from twisted.python.log import msg
-from twisted.python.threadable import isInIOThread
-from twisted.internet.defer import inlineCallbacks
-from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
-from twisted.internet.threads import deferToThread
+from twisted.python.log import msg
+
+from gumby.experiments.dispersyclient import DispersyExperimentScriptClient, main
+
 
 # TODO(emilon): Fix this crap
 pythonpath.append(path.abspath(path.join(path.dirname(__file__), '..', '..', '..', "./tribler")))
+
 
 class TunnelClient(DispersyExperimentScriptClient):
 
@@ -27,7 +26,8 @@ class TunnelClient(DispersyExperimentScriptClient):
         self.community_class = TunnelCommunity
 
         tunnel_settings = TunnelSettings()
-        tunnel_settings.max_circuits = 0
+        tunnel_settings.max_circuits = 8
+        tunnel_settings.socks_listen_port = -1
         self.set_community_kwarg('settings', tunnel_settings)
 
         self.monitor_circuits_lc = None
@@ -36,7 +36,41 @@ class TunnelClient(DispersyExperimentScriptClient):
     def registerCallbacks(self):
         self.scenario_runner.register(self.build_circuits, 'build_circuits')
 
+    def start_dispersy(self, autoload_discovery=True):
+        msg("Starting dispersy")
+        # We need to import the stuff _AFTER_ configuring the logging stuff.
+        from Tribler.dispersy.dispersy import Dispersy
+        from Tribler.community.tunnel.endpoint import TunnelStandaloneEndpoint
+        from Tribler.dispersy.util import unhandled_error_observer
+
+        self._dispersy = Dispersy(TunnelStandaloneEndpoint(int(self.my_id) + 12000, '0.0.0.0'), u'.', self._database_file, self._crypto)
+        self._dispersy.statistics.enable_debug_statistics(True)
+
+        self.original_on_incoming_packets = self._dispersy.on_incoming_packets
+
+        if self._strict:
+            from twisted.python.log import addObserver
+            addObserver(unhandled_error_observer)
+
+        self._dispersy.start(autoload_discovery=autoload_discovery)
+
+        if self.master_private_key:
+            self._master_member = self._dispersy.get_member(private_key=self.master_private_key)
+        else:
+            self._master_member = self._dispersy.get_member(public_key=self.master_key)
+        self._my_member = self._dispersy.get_member(private_key=self.my_member_private_key)
+        assert self._master_member
+        assert self._my_member
+
+        self._do_log()
+
+        self.print_on_change('community-kwargs', {}, self.community_kwargs)
+        self.print_on_change('community-env', {}, {'pid':getpid()})
+
+        msg("Finished starting dispersy")
+
     def build_circuits(self):
+        msg("build_circuits")
         self._community.settings.max_circuits = 8
 
     def online(self):
@@ -45,11 +79,14 @@ class TunnelClient(DispersyExperimentScriptClient):
             self.monitor_circuits_lc = lc = LoopingCall(self.monitor_circuits)
             lc.start(5.0, now=True)
 
+    def offline(self):
+        DispersyExperimentScriptClient.offline(self)
+
     def monitor_circuits(self):
         nr_circuits = len(self._community.circuits) if self._community else 0
         self._prev_scenario_statistics = self.print_on_change("scenario-statistics", self._prev_scenario_statistics, {'nr_circuits': nr_circuits})
 
-if __name__ == '__main__':
-    TunnelClient.scenario_file = environ.get('SCENARIO_FILE', 'tunnel.scenario')
-    main(TunnelClient)
 
+if __name__ == '__main__':
+    TunnelClient.scenario_file = 'tunnel.scenario'
+    main(TunnelClient)
