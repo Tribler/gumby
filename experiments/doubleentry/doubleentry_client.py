5d6c6ad9c0a48de12d383b99f3d008bc637027c7
Steffan Norberhuis <steffan@norberhuis.nl>
2015-04-08 13:29:25 +0200

    DoubleEntry: added client.

--- experiments/doubleentry/doubleentry_client.py
+++ experiments/doubleentry/doubleentry_client.py
@@ -8,39 +8,45 @@ from twisted.python.log import msg
 
 pythonpath.append(path.abspath(path.join(path.dirname(__file__), '..', '..', '..', "./tribler")))
 
+from Tribler.dispersy.candidate import Candidate
+
 
 class DoubleEntryClient(DispersyExperimentScriptClient):
 
     _SECURITY_LEVEL = u'high'
 
     def __init__(self, *argv, **kwargs):
-        print "DEC started"
         from Tribler.community.doubleentry.community import DoubleEntryCommunity
 
         msg("Starting DoubleEntry client")
         DispersyExperimentScriptClient.__init__(self, *argv, **kwargs)
         self.community_class = DoubleEntryCommunity
 
-        # self.set_community_kwarg('integrate_with_tribler', False)
-
     def registerCallbacks(self):
         self.scenario_runner.register(self.request_signature, 'request_signature')
         self.scenario_runner.register(self.draw_graph, 'draw_graph')
+        self.scenario_runner.register(self.close, 'close')
 
     def online(self):
-        print "DEC going online!"
+        msg("DEC going online!")
         DispersyExperimentScriptClient.online(self)
 
-    def request_signature(self):
-        print "Requesting Signature"
-        self._community.publish_signature_request_message()
+    def request_signature(self, candidate_id=0):
+        msg("%s: Requesting Signature for candidate: %s" % (self.my_id, candidate_id))
+        target = self.all_vars[candidate_id]
+        candidate = Candidate((str(target['host']), target['port']), False)
+        self._community.publish_signature_request_message(candidate)
 
     def draw_graph(self):
         from Tribler.community.doubleentry.experiments import GraphDrawer
 
-        print "Drawing graph"
+        msg("Drawing graph")
         graph_drawer = GraphDrawer(self.community.persistence)
         graph_drawer.draw_graph()
+
+    def close(self):
+        msg("close command received")
+        self._community.unload_community()
 
 if __name__ == '__main__':
     DoubleEntryClient.scenario_file = environ.get('SCENARIO_FILE', 'doubleentry.scenario')
