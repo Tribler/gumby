840c8afd8c02b9c3c452a557dcf253aee85de5d2
Elric Milon <whirm@gmx.com>
2015-06-22 12:42:51 +0200

    Fix sequential test execution.

--- scripts/run_nosetests_for_jenkins.sh
+++ scripts/run_nosetests_for_jenkins.sh
@@ -70,7 +70,7 @@ echo Nose will run from $PWD
 # TODO(emilon): Make the timeout configurable
 
 NOSEARGS_COMMON="--with-xunit --all-modules --traverse-namespace --cover-package=Tribler --cover-inclusive "
-NOSECMD="nosetests -v --with-xcoverage --xcoverage-file=$OUTPUT_DIR/coverage.xml --xunit-file=$OUTPUT_DIR/nosetests.xml $NOSEARGS_COMMON"
+NOSECMD="nosetests -v --with-xcoverage --xcoverage-file=$OUTPUT_DIR/coverage.xml --xunit-file=$OUTPUT_DIR/nosetests.xml.part $NOSEARGS_COMMON"
 
 export NOSE_LOGFORMAT="%(levelname)-7s %(created)d %(module)15s:%(name)s:%(lineno)-d4d %(message)s"
 
