54896e3a8dbdeb4e6bc316c0b630e552a496ee2f
Laurens Versluis <laurens-versluis@hotmail.com>
2016-07-20 12:20:21 +0200

    removed some debugging

--- experiments/dispersy/allchannel_client.py
+++ experiments/dispersy/allchannel_client.py
@@ -121,7 +121,6 @@ class AllChannelClient(DispersyExperimentScriptClient):
     @inlineCallbacks
     def publish(self, amount=1):
         amount = int(amount)
-        self._logger.error("in publish, 1")
         torrents = []
         if self.my_channel or self.joined_community:
             for _ in xrange(amount):
@@ -141,7 +140,6 @@ class AllChannelClient(DispersyExperimentScriptClient):
                 trackers = tuple(trackers)
 
                 self.torrentindex += 1
-                self._logger.error("in publish, 2")
                 torrents.append((infohash, int(time()), name, files, trackers))
         if torrents:
             if self.my_channel:
