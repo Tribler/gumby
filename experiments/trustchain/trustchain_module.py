f7e46c238a0408f05709111950db8b9252b27283
Martijn de Vos <mdmartijndevos@gmail.com>
2019-12-02 14:01:08 +0100

    Delaying when minting

--- experiments/trustchain/trustchain_module.py
+++ experiments/trustchain/trustchain_module.py
@@ -221,8 +221,10 @@ class TrustchainModule(IPv8OverlayExperimentModule):
 
         self._logger.info("Sending initial value to all peers...")
 
-        for peer in self.overlay.get_all_communities_peers():
-            self.transfer(peer, 1000)
+        peers = self.overlay.get_all_communities_peers()
+        for peer in peers:
+            delay = (1.0 / len(peers)) * int(self.experiment.get_peer_id(peer.address[0], peer.address[1]))
+            deferLater(reactor, delay, self.transfer, peer, 1000)
 
     @experiment_callback
     def start_creating_transactions(self):
