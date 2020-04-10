7a9d428d19f2c54842b14e111c90c6cb6ef5fe4d
Martijn de Vos <mdmartijndevos@gmail.com>
2020-04-10 18:01:20 +0200

    Added Algorand experiments

---
+++ experiments/algorand/generate_genesis.py
@@ -0,0 +1,42 @@
+import json
+
+NUM_VALIDATORS = 4
+
+genesis = {
+    "Genesis": {
+        "NetworkName": "",
+        "Wallets": [
+            # This is filled in
+        ]
+    },
+    "Nodes": [
+        # This is filled in
+    ]
+}
+
+stake_per_node = 100 // NUM_VALIDATORS
+last_node_stake = stake_per_node + 100 - (stake_per_node * NUM_VALIDATORS)
+
+for node_ind in range(NUM_VALIDATORS):
+    wallet_name = "Wallet%d" % (node_ind + 1)
+    stake = stake_per_node if node_ind != (NUM_VALIDATORS - 1) else last_node_stake
+
+    wallet_info = {
+        "Name": wallet_name,
+        "Stake": stake,
+        "Online": True
+    }
+    genesis["Genesis"]["Wallets"].append(wallet_info)
+
+    node_info = {
+        "Name": "Node%d" % (node_ind + 1),
+        "IsRelay": node_ind == 0,
+        "Wallets": [{
+            "Name": wallet_name,
+            "ParticipationOnly": False
+        }]
+    }
+    genesis["Nodes"].append(node_info)
+
+with open("genesis.json", "w") as genesis_file:
+    genesis_file.write(json.dumps(genesis))
