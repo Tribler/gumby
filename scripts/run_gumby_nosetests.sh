72c04840c2dd0417e76624926e5b8edf09038945
Martijn de Vos <mdmartijndevos@gmail.com>
2021-07-08 15:57:21 +0200

    Removed unused scripts

--- scripts/run_gumby_nosetests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-parentdir=$(dirname $0)
-cd $parentdir/../..
-PYTHONPATH=gumby:tribler:$PYTHONPATH nosetests2 --with-coverage --cover-html --cover-erase --cover-branches --cover-package=gumby --cover-package=experiments gumby/gumby/tests
