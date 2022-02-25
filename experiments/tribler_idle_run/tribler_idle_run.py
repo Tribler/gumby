a338ab723d66c6d0cb19effc6d66826c59ed72e8
Alexander Kozlovsky <alexander.kozlovsky@gmail.com>
2022-02-25 06:56:14 +0100

    Fix tribler_idle_run

--- experiments/tribler_idle_run/tribler_idle_run.py
+++ experiments/tribler_idle_run/tribler_idle_run.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
+import asyncio
 import os
 import signal
-from asyncio import ensure_future, get_event_loop, sleep
+import sys
 from pathlib import Path
 
 from tribler_core.components.base import Session
@@ -9,48 +10,47 @@ from tribler_core.config.tribler_config import TriblerConfig
 from tribler_core.start_core import components_gen
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+DEFAULT_EXECUTION_TIME = 60 * 10  # Run for 10 minutes by default
 
 
-async def idle_tribler_run():
+async def run_session(config, session):
+    signal.signal(signal.SIGTERM, lambda signum, stack: session.shutdown_event.set)
+    try:
+        async with session.start() as session:
+            if config.error:
+                session.logger.error(f'Config error: {config.error}')
+                sys.exit(-1)
+
+            session.logger.info("Tribler started")
+            await session.shutdown_event.wait()
+            session.logger.info("Stopping Tribler idle run")
+
+    except Exception as e:  # pylint: disable=broad-except
+        session.logger.exception(e)
+        raise
+
+
+async def end_session(session, execution_time):
+    await asyncio.sleep(execution_time)
+    session.shutdown_event.set()
+
+
+def idle_tribler_run():
     state_dir = Path(os.path.abspath(os.path.join(BASE_DIR, "output", "tribler-state")))
     config = TriblerConfig(state_dir=state_dir)
     config.api.http_enabled = False
     config.ipv8.port = 21000
     config.libtorrent.port = 21005
 
-    components = components_gen(config)
+    components = list(components_gen(config))
     session = Session(config, components)
-    signal.signal(signal.SIGTERM, lambda signum, stack: session.shutdown_event.set)
-    session.set_as_default()
 
-    try:
-        await session.start_components()
-    except Exception as e:  # pylint: disable=broad-except
-        print(str(e))
-        return
+    execution_time = int(os.environ.get("TRIBLER_EXECUTION_TIME", DEFAULT_EXECUTION_TIME))
 
-    print("Tribler started")
-
-    if "TRIBLER_EXECUTION_TIME" in os.environ:
-        run_time = int(os.environ["TRIBLER_EXECUTION_TIME"])
-    else:
-        run_time = 60 * 10  # Run for 10 minutes by default
-
-    await sleep(run_time)
-
-    print("Stopping Tribler idle run")
-    session.shutdown_event.set()
-
-    # Indicates we are shutting down core. With this environment variable set
-    # to 'TRUE', RESTManager will no longer accept any new requests.
-    os.environ['TRIBLER_SHUTTING_DOWN'] = "TRUE"
-
-    await session.shutdown()
-
-    get_event_loop().stop()
+    loop = asyncio.get_event_loop()
+    loop.create_task(end_session(session, execution_time))
+    loop.run_until_complete(run_session(config, session))
 
 
 if __name__ == "__main__":
-    ensure_future(idle_tribler_run())
-    get_event_loop().run_forever()
-    get_event_loop().close()
+    idle_tribler_run()
